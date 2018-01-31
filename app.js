44b14d5691b18e8112d5e2f3a62d2778a25bd075
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-30 22:23:02 -0200

    Handles the 'Invalid date' scenario

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const PORT = process.argv[2] || 3000;
 app.get("/:date", (req, res) => {
   try {
     const date = new Date(req.params.date);
+    const naturalDate = moment(date).format("MMMM DD, YYYY");
 
     const response = {
       unix: date.getTime() / 1000 || null,
-      natural: moment(date).format("MMMM DD, YYYY") || null
+      natural: !(!naturalDate || naturalDate == "Invalid date")
+        ? naturalDate
+        : null
     };
 
     res.json(response);
